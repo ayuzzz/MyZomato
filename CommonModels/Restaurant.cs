3da84df9a2c673bc54b9ad66b7e56035e370df06
Ayush Joshi <rockingayush1995@gmail.com>
2020-12-26 20:44:19 +0530

    Common models added

---
+++ CommonModels/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CommonModels
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public int CityId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int? Rating { get; set; }
+        public string Owner { get; set; }
+        public int? OpeningHour { get; set; }
+        public int? ClosingHour { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
