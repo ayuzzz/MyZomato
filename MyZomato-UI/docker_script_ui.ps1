0316f6921e8926246233359e7b3d9870a7fdef52
Ayush Joshi <rockingayush1995@gmail.com>
2021-04-04 18:18:48 +0530

    docker changes

---
+++ MyZomato-UI/docker_script_ui.ps1
@@ -0,0 +1,3 @@
+﻿docker build -t myzomato-ui -f DockerFile .
+docker run -dp 4200:80 myzomato-ui
+echo "`n`n---------------------------------------- myzomato-ui container is up and running---------------------------------------------------`n`n"
--- MyZomato-UI/src/app/core/uri/orders-api-uri.ts
+++ MyZomato-UI/src/app/core/uri/orders-api-uri.ts
@@ -1,3 +1,3 @@
 export class OrdersApiUri{
-    static CREATE_ORDER:string = "https://localhost:44337/api/Orders/create-order";
+    static CREATE_ORDER:string = "http://localhost:44337/api/Orders/create-order";
 }
