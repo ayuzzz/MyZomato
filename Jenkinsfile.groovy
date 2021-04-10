44eab92f61d1857a78aa29c05d5b6b7bd370a8e1
Ayush Joshi <joshiayush2426@gmail.com>
2021-04-10 17:16:33 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ node {
 
     docker.withRegistry('https://registry.hub.docker.com', 'dockerHub') {        
         
-        def customImage = docker.build("usermanagement-query", "-f usermanagementquery/Dockerfile .")
+        def customImage = docker.build("ayuzzz1995/usermanagement-query", "-f usermanagementquery/Dockerfile .")
 
         /* Push the container to the custom Registry */
         customImage.push()       
