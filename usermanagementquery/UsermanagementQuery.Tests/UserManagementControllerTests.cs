1c2ce7a209cb4b254c19f404b90ba0a57800d131
Ayush Joshi <rockingayush1995@gmail.com>
2021-05-01 17:09:03 +0530

    initial xunit project setup

---
+++ usermanagementquery/UsermanagementQuery.Tests/UserManagementControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UsermanagementQuery.Tests
+{
+    public class ControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
